c603dac065da646f79896051a6eabaaf2a33e0d3
prlps <modin.m30@yandex.ru>
2025-12-23 16:37:51 +0300

    add NonTarget Spell Effect

---
+++ Assets/Scripts/Untils/FixedRotation.cs
@@ -0,0 +1,30 @@
+using System.Numerics;
+using System.Threading.Tasks.Dataflow;
+using UnityEngine;
+
+public class FixedRotation : MonoBehaviour
+{
+
+    private Transform m_parent;
+    private Vector3 m_worldOffset;
+    private Quaternion m_rotation;
+
+    void Start()
+    {
+      m_parent = transform.parent;
+
+      m_rotation = transform.m_rotation;
+      m_worldOffset = transform.psition - m_parent.position;
+    }
+
+    void Update()
+    {
+        if (!m_parent)
+        {
+            return;
+        }
+
+        transform.psition = m_parent.position + m_worldOffset;
+        transform.rotation = m_rotation;
+    }
+}
